38150de34f5155c75ada13bf4d92db2412f9deb7
ravisankaliya <138698285+ravisankaliya@users.noreply.github.com>
2023-07-06 13:06:06 +0530

    new

---
+++ bmi.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+void main ()  // find out bmi.
+
+
+{
+    float weight = 55.55;
+    float height = 4.4;
+     float answer = 0;
+
+     answer = weight/(height*height);
+
+     printf("your answer is%f",answer);
+}
