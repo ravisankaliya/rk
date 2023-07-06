158611235051c69bf5d91b6604870e8941e1dd0c
ravisankaliya <138698285+ravisankaliya@users.noreply.github.com>
2023-07-06 12:56:53 +0530

    new

---
+++ circule.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()    // find out are of circule
+{
+  float pi = 3.14;
+  float reduce = 21;
+  float answer = 0;
+
+  answer = pi*reduce*reduce;
+
+  printf("your answer is %f",answer);
+}
