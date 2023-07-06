80fb8f2da9562aee43ffd988e433ff400769ec0d
ravisankaliya <138698285+ravisankaliya@users.noreply.github.com>
2023-07-06 12:34:29 +0530

    new

---
+++ rectangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main ()
+{
+    float length = 50.25;
+    float width = 75.50;
+    float answer = 0;
+
+    answer = length*width;
+
+    printf("your answer is %f", answer);
+}
