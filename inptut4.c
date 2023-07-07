bf7a05496624ae8ba1ef38fcefa3bb9efd5534ce
ravisankaliya <138698285+ravisankaliya@users.noreply.github.com>
2023-07-07 12:24:04 +0530

    new2

---
+++ inptut4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main ()
+{
+    int number1 = 5;
+    int number2=2;
+    float answer =0;
+
+    answer = number1/number2;
+    printf("the value of via  divison is %f",answer);
+
+    answer = number1%number2;
+    printf("\nthe value of via modulus is %f",answer);
+}
